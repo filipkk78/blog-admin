13954e9871b91d6be9a255ae2ddfeb4795249093
filipkk78 <filipkk78@gmail.com>
2025-04-15 11:37:05 +0200

    Add displaying comments and deleting them

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import styles from "./Dashboard.module.css";
 import { useEffect, useState } from "react";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router-dom";
-import { Pencil, Check, X } from "lucide-react";
+import { Pencil, Check, X, Trash2 } from "lucide-react";
 
 function Dashboard() {
   const [isTokenValid, setIsTokenValid] = useState(false);
@@ -10,6 +10,7 @@ function Dashboard() {
   const [posts, setPosts] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [comments, setComments] = useState(null);
 
   const token = localStorage.getItem("token");
   useEffect(() => {
@@ -48,27 +49,54 @@ function Dashboard() {
         return response.json();
       })
       .then((json) => setPosts(json.posts))
+      .catch((error) => setError(error));
+  }, [isTokenValid, token, posts]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/comments", {
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((json) => setComments(json.comments))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  }, [isTokenValid, token, posts]);
-  if (isTokenExpired)
+  }, [isTokenValid, token, comments]);
+  if (isTokenExpired) {
     return (
       <div>
         <h1>Token expired</h1> <Link to="/">Log back in to continue</Link>
       </div>
     );
+  }
   if (loading) return <Loading></Loading>;
-  if (error)
+  if (error) {
     return (
       <div>
         <h1>Access denied</h1> <Link to="/">Log in to continue</Link>
       </div>
     );
-
+  }
   function publishToggle(id) {
     fetch(`http://localhost:5000/api/posts/${id}`, {
       mode: "cors",
       method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  function deleteComment(id) {
+    fetch(`http://localhost:5000/api/comments/${id}`, {
+      mode: "cors",
+      method: "DELETE",
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -103,6 +131,34 @@ function Dashboard() {
                       onClick={() => publishToggle(post.id)}
                     >
                       <Pencil></Pencil>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h2>Comments</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Content</th>
+                <th>postId</th>
+                <th>Delete</th>
+              </tr>
+              {comments.map((comment) => (
+                <tr key={comment.id} className={styles.post}>
+                  <td>{comment.id}</td>
+                  <td>{comment.authorName}</td>
+                  <td>{comment.content}</td>
+                  <td>{comment.postId}</td>
+                  <td>
+                    <button
+                      onClick={() => deleteComment(comment.id)}
+                      className={styles.deleteBtn}
+                    >
+                      <Trash2></Trash2>
                     </button>
                   </td>
                 </tr>
